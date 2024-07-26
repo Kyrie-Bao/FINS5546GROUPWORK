65d78e7136868bafced01c59596a9089df246219
zid5529658ShujieSun <shujie158@outlook.com>
2024-07-26 17:49:46 +1000

    Change config
    Change config file to make it can be run on mac.
    Notice: My filed download from github   doesnot include pandas, install it if you meet same problem

--- project2/toolkit_config.py
+++ project2/toolkit_config.py
@@ -4,5 +4,12 @@ Project configuration file
 
 
 import os
-PRJDIR = 'C:\\Users\\DELL\\Documents\\GitHub\\FINS5546GROUPWORK'
+
+# Determine the home directory based on the operating system
+HOME_DIR = os.path.expanduser('~')
+
+# Project directory relative to the home directory
+PRJDIR = os.path.join(HOME_DIR, 'Documents', 'GitHub', 'FINS5546GROUPWORK')
+
+# Data directory within the project directory
 DATADIR = os.path.join(PRJDIR, 'data')
