25de0755dc5aaabc8e64d540aac8a289d8dbea77
z5537517miko <mikobayuaji@gmail.com>
2024-07-28 15:38:07 +1000

    edit the toolkit path

--- project2/toolkit_config.py
+++ project2/toolkit_config.py
@@ -7,6 +7,8 @@ import os
 
 HOME_DIR = os.path.expanduser('~')
 
+# HOME_DIR = 'C:\\Users\\mikob\\OneDrive'
+
 PRJDIR = os.path.join(HOME_DIR, 'Documents', 'GitHub', 'FINS5546GROUPWORK')
 
 DATADIR = os.path.join(PRJDIR, 'data')
